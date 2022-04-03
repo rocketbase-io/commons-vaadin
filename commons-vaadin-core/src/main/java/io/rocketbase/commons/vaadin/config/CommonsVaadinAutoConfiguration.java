e403d3a4e69a218bf3f6962d648c9d7faaa999dd
Marten <marten@rocketbase.io>
2022-04-03 16:17:10 +0200

    fix: corrected autoconfig + to register I18N Provider automatically

---
+++ commons-vaadin-core/src/main/java/io/rocketbase/commons/vaadin/config/CommonsVaadinAutoConfiguration.java
@@ -0,0 +1,20 @@
+package io.rocketbase.commons.vaadin.config;
+
+import com.vaadin.flow.i18n.I18NProvider;
+import io.rocketbase.commons.vaadin.i18n.TranslationProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ResourceLoader;
+
+@Configuration
+public class CommonsVaadinAutoConfiguration {
+
+    @Bean
+    @ConditionalOnMissingBean
+    public I18NProvider i18NProvider(@Autowired ResourceLoader resourceLoader) {
+        return new TranslationProvider(resourceLoader);
+    }
+
+}
